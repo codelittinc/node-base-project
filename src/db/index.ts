e3a5c2f5bb857b7d30d8974b6c16be28e58ea539
kaiomagalhaes <me@kaiomagalhaes.com>
2019-10-18 17:49:32 -0300

    add jest and create services tests example

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,12 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 import { loadEnvVars } from '../config/initializers/env_vars';
 
 loadEnvVars();
 
-const { DB_DATABASE = '', DB_USER = '', DB_PASSWORD = '' } = process.env;
+const { DB_DATABASE, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-export const database = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
-  dialect: "postgres",
+const dbConnURI = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/${DB_DATABASE}`;
+
+export const database = new Sequelize(dbConnURI, {
+  dialect: 'postgres'
 });
---
+++ tests/services/db/user.service.test.ts
@@ -0,0 +1,14 @@
+import UsersService from '../../../src/services/db/user.service';
+import { UserInterface } from '../../../src/models/user.model';
+
+describe('#create', () => {
+  it('returns a User with an id', () => {
+    const userParams: UserInterface = {
+      name: 'test name'
+    };
+
+    return UsersService.create(userParams).then(user => {
+      expect(user!.id).toBeTruthy();
+    });
+  });
+});
