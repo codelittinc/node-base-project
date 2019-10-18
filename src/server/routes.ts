c5684f1fd79c54af1b833f80ab060b92cab0b65e
kaiomagalhaes <me@kaiomagalhaes.com>
2019-10-18 10:26:21 -0300

    update lint

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,7 +1,7 @@
 import UsersController from '../controllers/user.controller'
 
 export function setupRoutes(app) {
-    // /users
-    app.get('/users', UsersController.getUsers)
-    app.post('/users', UsersController.createUser)
+  // /users
+  app.get('/users', UsersController.getUsers);
+  app.post('/users', UsersController.createUser);
 }
