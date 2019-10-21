2e48e2bd7a368782e1cd6e864044cdc283485173
kaiomagalhaes <me@kaiomagalhaes.com>
2019-10-21 18:41:14 -0300

    create base model structure and remove user service

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,7 +1,8 @@
-import { Model, DataTypes } from 'sequelize';
+import { DataTypes } from 'sequelize';
 import { database } from '@db';
+import BaseModel from './base.model';
 
-export class User extends Model {
+export class User extends BaseModel {
   public id!: number;
   public name!: string;
   public readonly createdAt!: Date;
@@ -25,7 +26,3 @@ User.init(
     sequelize: database
   }
 );
-
-export interface UserInterface {
-  name: string;
-}
