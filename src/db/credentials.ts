d843f622a59b46c8e58f29b293cd0ef8a97aec51
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2019-10-23 08:39:50 +0200

    Add configuration for database sql logs

--- src/db/credentials.ts
+++ src/db/credentials.ts
@@ -1,12 +1,13 @@
 import { loadEnvVars } from '@config/initializers/envVars';
 
 export function getDatabaseConfig() {
-  const { DB_DATABASE, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+  const { DB_DATABASE, DB_USER, DB_PASSWORD, DB_HOST, DB_SQLLOG } = process.env;
   const base = {
     user: DB_USER || 'postgres',
     pass: DB_PASSWORD || 'postgres',
     host: DB_HOST,
-    databaseName: DB_DATABASE || 'node_graphql'
+    databaseName: DB_DATABASE || 'node_graphql',
+    sqlLog: DB_SQLLOG === 'true'
   };
 
   const development = {
