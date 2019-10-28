c57d1e915d472feafe31419b6d6d5d2be527428f
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2019-10-28 18:52:12 +0100

    Add Typescript Rest and swagger documentation

---
+++ src/controllers/docs/user.ts
@@ -0,0 +1,8 @@
+import { IUser } from '@controllers/types/user';
+
+export const userExample: IUser = {
+  id: 1,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  name: 'Joe'
+};
--- src/models/base.model.ts
+++ src/models/base.model.ts
@@ -1,18 +1,21 @@
 import { Model, UpdateOptions, DestroyOptions } from 'sequelize';
 
 abstract class BaseModel extends Model {
-  public static async updateOne(
-    instance: BaseModel
-  ): Promise<[number, BaseModel[] | null]> {
+  public static async updateOne(instance: any): Promise<BaseModel | null> {
     const updateOpts: UpdateOptions = {
-      where: { id: (instance as any).id },
-      limit: 1
+      where: { id: instance.id },
+      limit: 1,
+      returning: true
     };
 
-    return (this as any).update(instance, updateOpts);
+    const result = await (this as any).update(instance, updateOpts);
+    if (result[0]) {
+      return result[1][0];
+    }
+    return null;
   }
 
-  public static async deleteOne(id: number) {
+  public static async deleteOne(id: number): Promise<number> {
     const deleteOpts: DestroyOptions = {
       where: { id },
       limit: 1
