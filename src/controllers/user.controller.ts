71de11484055d196c5ac1ec481e84eb5f4c1ec9b
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2019-10-28 18:52:12 +0100

    Add Typescript Rest and swagger documentation

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,35 +1,90 @@
-import { Request, Response } from 'express';
 import { User } from '@models/user.model';
+import {
+  Path,
+  GET,
+  PathParam,
+  Errors,
+  POST,
+  PATCH,
+  DELETE
+} from 'typescript-rest';
+import { Response, Produces, Example } from 'typescript-rest-swagger';
+import { NotFoundError } from 'typescript-rest/dist/server/model/errors';
+import { userExample } from './docs/user';
+import { IUser } from './types/user';
+import { CountResponse } from './types/common';
 
-export default class UsersController {
-  static async list(_: Request, res: Response) {
-    res.send(await User.findAll());
+@Path('/users')
+@Produces('application/json')
+export class UsersController {
+  /**
+   * Recovers all active users
+   */
+  @GET
+  @Response<IUser[]>(200, 'Retrieve a list of users.')
+  @Example<Array<IUser>>([userExample])
+  async list() {
+    return await User.findAll();
   }
 
-  static async show(req: Request, res: Response) {
-    const userId: number = parseInt(req.params.id);
-    const user = await User.findByPk(userId);
+  /**
+   * Recovers the user resource by its id
+   * @param id user primary identifier
+   */
+  @Path('/:id')
+  @GET
+  @Response<IUser>(200, 'Retrieve a user.')
+  @Response<NotFoundError>(404, 'User not found')
+  @Example<IUser>(userExample)
+  async show(@PathParam('id') id: number): Promise<IUser> {
+    const user = await User.findByPk(id);
     if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ errors: ['User not found'] });
+      return user;
     }
+    throw new Errors.NotFoundError('User not found');
   }
 
-  static async create(req: Request, res: Response) {
-    const user: object = req.body;
-    res.send(await User.create(user));
+  /**
+   * Creates a user
+   */
+  @POST
+  @Response<IUser>(201, 'Created user')
+  @Example<IUser>(userExample)
+  async create(user: IUser) {
+    return await User.create(user);
   }
 
-  static async update(req: Request, res: Response) {
-    const user = req.body;
-    await User.updateOne(user);
-    res.json();
+  /**
+   * Updates the user
+   * @param id user primary identifier
+   */
+  @Path('/:id')
+  @PATCH
+  @Example<IUser>(userExample)
+  @Response<IUser>(200, 'Update the user that was sent')
+  async update(
+    @PathParam('id') id: number,
+    user: IUser
+  ): Promise<IUser | null> {
+    const result = await User.updateOne({ ...user, id });
+    if (result) {
+      return result as any;
+    }
+    throw new Errors.NotFoundError('User not found');
   }
 
-  static async delete(req: Request, res: Response) {
-    const userId: number = parseInt(req.params.id);
-    await User.deleteOne(userId);
-    res.json();
+  /**
+   * Delete the user by its id
+   * @param id user primary identifier
+   */
+  @Path('/:id')
+  @DELETE
+  @Response<CountResponse>(200, 'User was deleted')
+  async delete(@PathParam('id') id: number): Promise<CountResponse> {
+    const result = await User.deleteOne(id);
+    if (result) {
+      return new CountResponse(result);
+    }
+    throw new Errors.NotFoundError('User not found');
   }
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,21 +1,54 @@
 import * as express from 'express';
+import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
-import { setupRoutes } from './routes';
+import { Server, Errors } from 'typescript-rest';
+import * as swaggerUi from 'swagger-ui-express';
+import * as Controllers from '@controllers/index';
+
+const swaggerDocument = require('../../swagger.json');
+
+const exceptionResolver = (
+  err: any,
+  _req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  if (err instanceof Errors.HttpError) {
+    if (res.headersSent) {
+      // important to allow default error handler to close connection if headers already sent
+      return next(err);
+    }
+    res.set('Content-Type', 'application/json');
+    res.status(err.statusCode);
+    res.json({ error: err.message, code: err.statusCode });
+  } else {
+    next(err);
+  }
+};
 
 export function getApp() {
   const { SERVER_PARSER_EXTENDED, SERVER_PARSER_LIMIT } = process.env;
 
-  const app = express();
+  const app: express.Application = express();
   app.use(
     bodyParser.urlencoded({
       extended: SERVER_PARSER_EXTENDED === 'true',
       limit: SERVER_PARSER_LIMIT || '2mb'
     })
   );
+  app.use(cors());
 
   app.use(bodyParser.json());
 
-  setupRoutes(app);
+  // Setup swagger
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use('/docs.json', (_req, res) => res.send(swaggerDocument));
+
+  Object.values(Controllers).map(controller => {
+    Server.buildServices(app, controller);
+  });
+
+  app.use(exceptionResolver);
 
   return app;
 }
