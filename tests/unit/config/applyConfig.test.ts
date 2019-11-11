a8cb8ccd5b584e0613af5b7c6f4af941ad74f64c
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2019-11-11 08:38:01 +0100

    Add mock tests examples on unit tests

---
+++ tests/unit/config/applyConfig.test.ts
@@ -0,0 +1,17 @@
+import { applyConfig } from '@config';
+import { start } from '../../../src';
+
+jest.mock('@config', () => ({
+  applyConfig: jest.fn(),
+  getConfig: () => ({
+    database: {
+      sqlLog: false
+    }
+  })
+}));
+
+it('On startup, calls the applyConfig', async () => {
+  const server = start();
+  expect(applyConfig).toHaveBeenCalled();
+  server.close();
+});
