36e183dc186f16bb9f6932e1143e4a5c8c6b1777
Victor <vcarvalho0402@gmail.com>
2019-11-13 10:48:13 -0300

    Fix the docker NPM scripts
    Rename the docker-compose yml file for dev to use the default.
    
    Add a new .env variable to allow the user to change the postgres port that will be exposed to the host.
    
    And add the .idea folder to .gitignore.

--- scripts/docker/start.sh
+++ scripts/docker/start.sh
@@ -1,4 +1,4 @@
-docker-compose -f docker-compose.dev.yml up -d
+docker-compose up -d
 docker exec -it node-graphql-app npm run db:create:all
 docker exec -it node-graphql-app npm run start:dev
 docker logs node-graphql-app --follow
