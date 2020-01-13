76e8f66458cc7ad55860031a3db912071a078943
Vincent Gschwend <vincent@crypta.li>
2020-01-13 15:03:38 +0000

    Remove unnecessary space in index.ts

--- scripts/generator/index.ts
+++ scripts/generator/index.ts
@@ -8,7 +8,7 @@ import { Test } from './actions/test';
 cmd
   .version('0.0.1')
   .arguments('<name> <referenceName> [customMigrationFileName]')
-  .description('Generate all files necessary to create a new model ')
+  .description('Generate all files necessary to create a new model')
   .option(
     '--foreign',
     'add this flag if your model uses constraints, based on referenceName',
